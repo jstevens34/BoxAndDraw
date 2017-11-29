4136acaae8d9e26358d0c3e270cef813dd8e71c6
Joe Stevens <joseph.stevens34@gmail.com>
2017-11-28 23:00:32 -0500

    first commit

---
+++ app/src/main/java/com/example/joe/draganddraw/DragAndDrawFragment.java
@@ -0,0 +1,25 @@
+package com.example.joe.draganddraw;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Joe on 11/28/2017.
+ */
+
+public class DragAndDrawFragment extends Fragment{
+
+    public static DragAndDrawFragment newInstance() {
+        return new DragAndDrawFragment();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_drag_and_draw, container, false);
+        return v;
+    }
+}
